8f03fe9ca112b375ba8ce455bbc9778bda406399
lahirunanayakkaraseer <lahiru.nanayakkara@seer365.com>
2025-09-29 14:32:35 +0530

    Lisence block fetching updated

--- src/api/fetchLicenseBlocks.js
+++ src/api/fetchLicenseBlocks.js
@@ -18,67 +18,80 @@ export const fetchLicenseBlocks = async (newOriginURL, surveySlogan) => {
     if (response.data) {
       return response.data;
     } else {
-      var surveyQuestionModule = await import(
-        `${import.meta.env.VITE_AZURE_BLOB_URL}/${surveySlogan}/index.js`
-      );
-      if (surveyQuestionModule && surveyQuestionModule.surveyQuestionsJSON) {
-        return JSON.parse(surveyQuestionModule.surveyQuestionsJSON);
-      }
+      throw new Error("Dataverse don't have data for given url");
     }
-    throw new Error("Neither data from dataverse nor blob file were found");
   } catch (error) {
+    if (error.response.data === "Dataverse don't have data for given url") {
+      return await fetchLicenseBlocksFromBlob(surveySlogan);
+    }
     console.error("Error fetching license blocks:", error);
     throw error;
   }
 };
 
-export async function checkLicensingBlock() {
-  // Encode the FetchXML
-  var fetchXml = [
-    "<fetch returntotalrecordcount='true'>",
-    "  <entity name='seer_licensingblock'>",
-    "    <link-entity name='seer_surveys' from='seer_licensingblock' to='seer_licensingblockid'>",
-    "      <attribute name='seer_licensingblock' />",
-    "      <filter>",
-    "        <condition attribute='seer_neworiginurl' operator='eq' value='https://survey-portal-uat-gxchbpcrc4fkbze3.uksouth-01.azurewebsites.net/survey/lahiru-training-bc' />",
-    "      </filter>",
-    "    </link-entity>",
-    "  </entity>",
-    "</fetch>",
-  ].join("");
-
-  var query = "?fetchXml=" + encodeURIComponent(fetchXml);
-
-  // Call Dataverse Web API
+async function fetchLicenseBlocksFromBlob(surveySlogan) {
   try {
-    const response = await fetch(
-      `https://org3ea1142d.api.crm11.dynamics.com/api/data/v9.2/seer_licensingblocks${query}`,
-      {
-        method: "GET",
-        headers: {
-          // Authorization: `Bearer ${accessToken}`,
-          "OData-MaxVersion": "4.0",
-          "OData-Version": "4.0",
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      }
+    var surveyQuestionModule = await import(
+      `${
+        import.meta.env.VITE_AZURE_BLOB_URL
+      }/${surveySlogan}/surveyQuestionJSON.js`
     );
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-
-    console.log("Records found: " + data.value.length);
-
-    if (data.value.length > 0) {
-      // Call function if records exist
-    } else {
-      // Call function if no records exist
+    if (surveyQuestionModule && surveyQuestionModule.surveyQuestionsJSON) {
+      return surveyQuestionModule.surveyQuestionsJSON;
     }
   } catch (error) {
-    console.log(error);
+    console.error("Error fetching license blocks from blob:", error);
+    throw error;
   }
 }
+
+// export async function checkLicensingBlock() {
+//   // Encode the FetchXML
+//   var fetchXml = [
+//     "<fetch returntotalrecordcount='true'>",
+//     "  <entity name='seer_licensingblock'>",
+//     "    <link-entity name='seer_surveys' from='seer_licensingblock' to='seer_licensingblockid'>",
+//     "      <attribute name='seer_licensingblock' />",
+//     "      <filter>",
+//     "        <condition attribute='seer_neworiginurl' operator='eq' value='https://survey-portal-uat-gxchbpcrc4fkbze3.uksouth-01.azurewebsites.net/survey/lahiru-training-bc' />",
+//     "      </filter>",
+//     "    </link-entity>",
+//     "  </entity>",
+//     "</fetch>",
+//   ].join("");
+
+//   var query = "?fetchXml=" + encodeURIComponent(fetchXml);
+
+//   // Call Dataverse Web API
+//   try {
+//     const response = await fetch(
+//       `https://org3ea1142d.api.crm11.dynamics.com/api/data/v9.2/seer_licensingblocks${query}`,
+//       {
+//         method: "GET",
+//         headers: {
+//           // Authorization: `Bearer ${accessToken}`,
+//           "OData-MaxVersion": "4.0",
+//           "OData-Version": "4.0",
+//           Accept: "application/json",
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     );
+
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+
+//     const data = await response.json();
+
+//     console.log("Records found: " + data.value.length);
+
+//     if (data.value.length > 0) {
+//       // Call function if records exist
+//     } else {
+//       // Call function if no records exist
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
